c9ec5a357c14569b2aa1914c914c104eaebf5909
pravalliika <medisettipravallika8@gmail.com>
2026-02-15 17:40:40 +0530

    varibales-declared

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,3 +3,4 @@
 echo "Trump:: Hey Musk ,How are you?"
 echo "Musk:: Hello Trump,I am fine."
 echo "Trump:: Nothing,just going to Mars"
+echo "Musk:: OK,carryon bye!"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: Hey $PERSON2 ,How are you?"
+echo "$PERSON2:: Hello $PERSON1,I am fine."
+echo "$PERSON1:: Nothing,just going to Mars"
+echo "$PERSON2:: OK,carryon bye!"
