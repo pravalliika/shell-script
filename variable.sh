98515a00981249feb372364ac4f628be80e4006a
pravalliika <medisettipravallika8@gmail.com>
2026-02-18 19:34:11 +0530

    variables pin

--- variable.sh
+++ variable.sh
@@ -10,3 +10,18 @@ rocket-start-engine $mission_name
 rocket-lift-off $mission_name
 
 rocket-status $mission_name    ##:%s/lunar-mission/mission_name/g
+
+
+
+# echo "Capital city of $1 is $2"
+
+# This script creates a backup of a given file by creating a copy as bkp
+# For example some-file is backed up as some-file_bkp
+# set -e
+
+# file_name=$1
+
+# cp $file_name ${file_name}_bkp
+
+# echo "Done"
+~                                 
